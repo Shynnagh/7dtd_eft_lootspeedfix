b9ba61b8b2ab45cb6a87c2e3793472d30a853b4e
lukas.meck <lukas.meck@gmail.com>
2025-07-30 14:16:01 +0200

    Add CVAR hook for Lucky Looter Action Skill on server side

---
+++ ActionSkillHook/ActionSkillHook/LuckyLooterHook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ActionSkillHook
+{
+    [HarmonyPatch(typeof(LootManager), "LootContainerOpened")]
+    public class LuckyLooterHook
+    {
+        public static void Postfix(ITileEntityLootable _tileEntity, int _entityIdThatOpenedIt, FastTags<TagGroup.Global> _containerTags)
+        {
+            if (_tileEntity.bPlayerStorage || _tileEntity.bWasTouched)
+                return;
+
+            EntityPlayer player = GameManager.Instance.World.GetEntity(_entityIdThatOpenedIt) as EntityPlayer;
+            if (player == null) return;
+            
+            const string cvar_loot_counter_name = "$ASkill_General_OpenedLootContainers";
+            const string cvar_loot_speed_name = "$ASkill_General_LootContainerOpenSpeedBonus";
+            
+            float current_count = player.Buffs.GetCustomVar(cvar_loot_counter_name);         
+            player.Buffs.SetCustomVar(cvar_loot_counter_name, current_count + 1f);
+            Debug.Log($"[LuckyLooterHook] {cvar_loot_counter_name} = {current_count:0}");
+
+            float speed_bonus = Math.Min((current_count + 1f) * 0.00016f, 0.8f);
+            player.Buffs.SetCustomVar(cvar_loot_speed_name, speed_bonus);
+            Debug.Log($"[LuckyLooterHook] {cvar_loot_speed_name} = {speed_bonus:0.000}");
+
+        }
+    }
+}
---
+++ ActionSkillHook/ActionSkillHook/ModInit.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ActionSkillHook
+{
+    public class ModInit : IModApi
+    {
+        private static Mod _mod_instance;
+        private static Harmony _harmony_instance;
+
+        public static Mod ModInstance => _mod_instance;
+
+        public static Harmony HarmonyInstance
+        {
+            get
+            {
+                return _harmony_instance ?? (_harmony_instance = new Harmony("com.reynard.actionskillhook"));
+            }
+        }
+        public void InitMod(Mod mod_instance)
+        {
+            _mod_instance = mod_instance;
+            HarmonyInstance.PatchAll();
+                
+            Debug.Log("[ActionSkillHook] all patched");                        
+        }
+    }
+}
